ef3b35d4d81e7eeb400f25a74de92a09da3f98c4
Jesse Chen <livingston.chen@gmail.com>
2014-07-18 15:01:00 +0800

    King: add test code for (ThreadSafe)Sequence.

--- src/main/java/com/aries/tiger/concurrent/chapter1/Sequence.java
+++ src/main/java/com/aries/tiger/concurrent/chapter1/Sequence.java
@@ -1,15 +1,46 @@
 package com.aries.tiger.concurrent.chapter1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.aries.tiger.concurrent.common.GuardedBy;
 import com.aries.tiger.concurrent.common.ThreadSafe;
 
 @ThreadSafe
 public class Sequence {
 
+	static Logger log = LoggerFactory.getLogger(Sequence.class);
+
 	@GuardedBy("this")
 	private int value;
-	
-	public synchronized int getNext(){
+
+	public synchronized int getNext() {
 		return value++;
+	}
+
+	public static void main(String[] args) {
+		final Sequence seq = new Sequence();
+		Thread tA = new Thread() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					log.debug("Thread A: {}", seq.getNext());
+				}
+			}
+		};
+
+		Thread tB = new Thread() {
+
+			@Override
+			public void run() {
+				for (int i = 0; i < 10; i++) {
+					log.debug("Thread B: {}", seq.getNext());
+				}
+			}
+		};
+
+		tA.start();
+		tB.start();
 	}
 }
